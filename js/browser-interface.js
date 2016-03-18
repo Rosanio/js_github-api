3e15ef3bc7ec082c847513de67b9dc8f55c6b52e
Matt Rosanio <mmrosani@syr.edu>
2016-03-18 10:30:32 -0700

    Add functionality to hide title of repo list and empty repo list when a new user is searched

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,11 +1,14 @@
 var getRepos = require('./../js/get-repos.js').getRepos;
+var getUser = require('./../js/get-user.js').getUser;
 var apiKey = require('./../.env').apiKey;
 
 $(function() {
   $('#githubUsername').submit(function(event) {
     event.preventDefault();
+    $('#repoList').empty();
     var username = $('#inputUsername').val();
+    var user = getUser(username);
     var repos = getRepos(username);
-
+    $('#username').show();
   });
 });
--- js/get-repos.js
+++ js/get-repos.js
@@ -1,22 +1,20 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(username) {
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
-    console.log(response);
-  }).fail(function(error) {
-    console.log(error.responseJSON.message);
-  });
+  //MAKE REQUEST TO GITHUB
   $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + '&per_page=1000').then(function(response) {
-    console.log(response);
+    //LIST THROUGH EACH REPO OBJECT
     for(var i = 0; i < response.length; i++) {
+      //DISPLAY NAME
       $('.repoList>dl').append('<dt>' + response[i].name + '</dt>');
+      //GRAB THE DATE AND TIME
       var date = response[i].created_at.substring(0,10);
       var time = response[i].created_at.substring(11,19);
       var dateAndTime = date + ' ' + time;
+      //CONVERT DATE AND TIME TO AM/PM AND FROM GMT TO PACIFIC TIME ZONE USING FORMAT AND SUBTRACT METHODS, RESPECTIVELY
       var momentDateTime = moment(dateAndTime).subtract(7, 'hours').format('MM/DD/YYYY hh:mm:ss a');
-      console.log(dateAndTime)
-      console.log(momentDateTime);
-      $('.repoList>dl').append('<dd>Created at ' + momentDateTime + '</dd>');
+      $('.repoList>dl').append('<dd>Created: ' + momentDateTime + '</dd>');
+      //LIST DESCRIPTION IF ONE EXISTS
       if(response[i].description !== null) {
         if(response[i].description.length > 0) {
           $('.repoList>dl').append('<dd>' + response[i].description + '</dd>');
