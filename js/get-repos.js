554d519b5efd8ad54a328f786ff5e87efc2121da
Matt Rosanio <mmrosani@syr.edu>
2016-03-18 09:07:04 -0700

    Add functionality to getRepos function to display description for repo if one exists

--- js/get-repos.js
+++ js/get-repos.js
@@ -4,9 +4,14 @@ exports.getRepos = function(username) {
   $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey + '&per_page=1000').then(function(response) {
     console.log(response);
     for(var i = 0; i < response.length; i++) {
-      $('.repoList>ul').append('<li>' + response[i].name + '</li>');
+      $('.repoList>dl').append('<dt>' + response[i].name + '</dt>');
+      if(response[i].description !== null) {
+        if(response[i].description.length > 0) {
+          $('.repoList>dl').append('<dd>' + response[i].description + '</dd>');
+        }
+      }
     }
   }).fail(function(error) {
     console.log(error.responseJSON.message);
   });
-}
+};
