f9fe1fbd549df06f5653ea81d21fd60ab3bdaec2
Matt Rosanio <mmrosani@syr.edu>
2016-03-18 08:13:00 -0700

    Add function to log response from api request, currently failing

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,3 +1,10 @@
-$(function() {
+var apiKey = require('./../.env').apiKey;
 
+$(function() {
+  console.log(apiKey);
+  $.get('https://api.github.com/users/Rosanio/repos?access_token=' + apiKey).then(function(response) {
+    console.log(response);
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
 });
